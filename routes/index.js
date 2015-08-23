c82deaaac960e0c616ed8b1aa422b32704dbd17d
Fabian Roloff <fabian.roloff@vocatus.de>
2015-08-23 08:43:38 +0200

    change app name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Vocatus' });
+    res.render('index', { title: 'respi-web-control' });
 });
 
 module.exports = router;
